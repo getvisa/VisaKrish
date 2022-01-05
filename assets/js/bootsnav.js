74fa3475bbebea18df7122895a72bcbfd8ed9224
getvisa <getvisa@gmail.com>
2022-01-05 17:18:05 +0530

    Update bootsnav.js

--- assets/js/bootsnav.js
+++ assets/js/bootsnav.js
@@ -12,6 +12,23 @@
 (function ($) {
 	"use strict";
     
+   /**
+   * Hero type effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
+
     var bootsnav = {
         initialize: function() {
             this.event();
